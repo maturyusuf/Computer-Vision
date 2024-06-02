e774fd83c3c951edf73251b2e78870c64926fe7e
Yusuf Matur <75329717+maturyusuf@users.noreply.github.com>
2024-06-02 16:40:10 +0300

    Add files via upload

--- computer-vision-tutorials/Image_Processing_filtering.py
+++ computer-vision-tutorials/Image_Processing_filtering.py
@@ -1,3 +1,26 @@
 import cv2
 import numpy as np
 
+cap = cv2.VideoCapture(0)
+
+while(1):
+    _, frame = cap.read()
+    # Converting from BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    lower_beige = np.array([34,0,0 ])
+    upper_beige = np.array([100,255,255 ])
+    
+    mask = cv2.inRange(hsv, lowerb=lower_beige, upperb=upper_beige)
+    result = cv2.bitwise_and(frame, frame, mask = mask)
+    
+    cv2.imshow("frame", frame)
+    cv2.imshow("mask", mask)
+    cv2.imshow("result", result)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+cap.release()
+
